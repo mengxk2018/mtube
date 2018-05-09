fe50a431458e900e356cc30274f0776af22772a5
xiankai <mengxiankai@flashltd.net>
2018-05-09 16:00:07 +0800

    init commit

---
+++ Player/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml;
+
+namespace Player
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) return;
+
+            try
+            {
+                var source = "";
+                var player = "";
+                var config = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "web.config");
+                if (File.Exists(config))
+                {
+                    var xml = new XmlDocument();
+                    xml.Load(config);
+
+                    var xnSource = xml.SelectSingleNode("//add[@key='source']");
+                    if (xnSource != null) source = xnSource.Attributes["value"].Value;
+
+                    var xnPlayer = xml.SelectSingleNode("//add[@key='player']");
+                    if (xnPlayer != null) player = xnPlayer.Attributes["value"].Value;
+                }
+
+                var videoName = HttpUtility.UrlDecode(args[0].Replace("player://", "").Replace("movie/", "").Replace("/", ""));
+                var video = Path.Combine(source, videoName);
+                if (File.Exists(video))
+                {
+                    if (File.Exists(player))
+                    {
+                        Process.Start(player, video);
+                    }
+                    else
+                    {
+                        Process.Start(video);
+                    }
+                }
+            }
+            catch { }
+        }
+    }
+}
---
+++ Video/Video.ashx.cs
@@ -0,0 +1,100 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Video
+{
+    public class Video : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            var response = context.Response;
+            var request = context.Request;
+            var result = "";
+            var type = request["type"] ?? "";
+            switch (type)
+            {
+                case "load":
+                    result = GetFiles();
+                    break;
+                case "delete":
+                    var deleteFile = request["file"] ?? "";
+                    result = DeleteFile(deleteFile);
+                    break;
+            }
+            response.Write(result);
+            response.End();
+        }
+
+        private string GetFiles()
+        {
+            var result = "";
+            try
+            {
+                var source = Sources;
+                var files = Directory.GetFiles(source, "*.*", SearchOption.AllDirectories);
+                files = files.Where(file => file.EndsWith(".mp4", StringComparison.CurrentCultureIgnoreCase) || file.EndsWith(".mkv", StringComparison.CurrentCultureIgnoreCase) || file.EndsWith(".rmvb", StringComparison.CurrentCultureIgnoreCase)).OrderBy(file => new FileInfo(file).LastWriteTime).Reverse().ToArray();
+                result = files.ToJson();
+            }
+            catch { }
+            return result;
+        }
+
+        private string DeleteFile(string file)
+        {
+            var result = "";
+            try
+            {
+                var delete = HttpContext.Current.Request.Cookies["delete"];
+                if (delete == null) return result;
+
+                var path = Path.Combine(Sources, file);
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                    result = "success";
+                }
+            }
+            catch { }
+            return result;
+        }
+
+        public string Sources
+        {
+            get { return ConfigurationManager.AppSettings["source"]; }
+        }
+
+        public string Player
+        {
+            get { return ConfigurationManager.AppSettings["player"]; }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+
+    public static class JsonHelper
+    {
+        public static string ToJson<T>(this T data)
+        {
+            var json = JsonConvert.SerializeObject(data);
+            return json;
+        }
+
+        public static T ToObject<T>(this string value)
+        {
+            return JsonConvert.DeserializeObject<T>(value);
+        }
+    }
+}
